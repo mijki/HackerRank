a49328e68a238f4ff68ee5b45f9767d5aff000ac
Miklós Kiss <mijki95@gmail.com>
2022-09-18 11:50:22 +0200

    BirthdaytCake and ClockConverter

---
+++ BirthdayCake/Program.cs
@@ -0,0 +1,29 @@
+﻿/*  You are in charge of the cake for a child's birthday.
+    You have decided the cake will have one candle for each year of their total age. 
+    They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
+
+    Returns
+    int: the number of candles that are tallest 
+*/
+
+using System;
+
+List<int> inputData = new List<int>() {3, 2, 1, 3};
+
+int maxNum = 0, count = 0;
+inputData.Sort();
+
+for (int i = 0; i < inputData.Count; i++)
+{
+    if (inputData[i] > maxNum)
+    {
+        maxNum = inputData[i];
+        count = 1;
+    }
+    else if (inputData[i] == maxNum)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"{maxNum} {count}");
---
+++ ClockConverter/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Given a time in -hour AM/PM format, convert it to military (24-hour) time.
+
+    Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
+    - 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
+
+    Function Description
+    Complete the timeConversion function in the editor below. It should return a new string representing the input time in 24 hour format.
+
+    timeConversion has the following parameter(s):
+    string s: a time in  hour format
+
+    Returns
+    string: the time in  hour format
+*/
+
+using System.Threading.Tasks.Dataflow;
+
+string sample1 = "12:23:23AM";
+string sample2 = "07:23:52PM";
+
+TimeTo24HourClock(sample1);
+TimeTo24HourClock(sample2);
+
+
+
+static string TimeTo24HourClock(string s)
+{
+    string sCopy = s.Remove(8,2);
+    string[] tempString = sCopy.Split(':');
+
+    if (s.Contains("AM"))
+    {
+        if (tempString[0] == "12") tempString[0] = "00";
+    }
+
+    if (s.Contains("PM"))
+    {
+        if (tempString[0] != "12")
+        {
+            tempString[0] = Convert.ToString(Convert.ToInt32(tempString[0]) + 12);
+        }
+    }
+
+    sCopy = string.Join(":", tempString);
+    Console.WriteLine(sCopy);
+    return sCopy;
+}
